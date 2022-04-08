058fd2a265b70353c2b542d40a45f28b2a1c7c01
jaswanthkaviti <neikavi3599@gmail.com>
2022-04-08 22:23:53 +0530

    basic_java

---
+++ calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class calculator{
+	public static void main(String[] args) {
+		System.out.println("enter the values");
+		Scanner input = new Scanner(System.in);
+		double d1 = input.nextDouble();
+		char op = input.next().charAt(0);
+		double d2 = input.nextDouble();
+		
+		if(op == '+')
+			System.out.println(d1 + d2);
+		else if(op == '-')
+			System.out.println(d1 - d2);
+		else if(op == '*')
+			System.out.println(d1 * d2);
+		else if(op == '/')
+			System.out.println(d1 / d2);
+		else if(op == '%')
+			System.out.println(d1 % d2);
+		input.close();
+	}
+}
