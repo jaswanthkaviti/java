7932118a93017ca24ea203b49ced9710e505af9b
jaswanthkaviti <neikavi3599@gmail.com>
2022-04-10 01:13:20 +0530

    nested loop

---
+++ multiplication.java
@@ -0,0 +1,13 @@
+public class multiplication{
+	public static void main(String[] args) {
+		int i,j;
+		
+		for(i = 1; i <= 10; i++) {
+			for(j = 1; j <=10; j++) {
+				System.out.print((i*j) + " ");
+			}
+			System.out.println();
+		
+		}
+	}
+}
