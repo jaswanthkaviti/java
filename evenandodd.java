2aeaa510635f046b101f779c0149195f67b56124
jaswanthkaviti <neikavi3599@gmail.com>
2022-04-10 01:41:59 +0530

    using_for_loop

---
+++ evenandodd.java
@@ -0,0 +1,20 @@
+
+public class multiplication{
+	public static void main(String[] args) {
+		int i;
+		System.out.println("even numbers");
+		for(i=1 ; i<=100 ; i++) {
+			if(i%2 == 0) {
+				System.out.println(i + " ");
+			}
+		}	 
+       System.out.println("odd numbers");
+		for(i=1 ; i<=100 ; i++) {
+			if(i%2 == 1) {
+				System.out.println(i + " ");
+			}
+			
+		}
+		
+	}
+}
