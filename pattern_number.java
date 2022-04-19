ac3a01aaa2d78ac69e6c120a135398c1266c8014
jaswanthkaviti <neikavi3599@gmail.com>
2022-04-19 22:30:22 +0530

    related to loops

---
+++ pattern_number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("enter maximum number: ");
+		int n = input.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1; j<=i ; j++)
+				System.out.print(i);
+			
+			System.out.println();
+		}
+		input.close();
+	}
+}
